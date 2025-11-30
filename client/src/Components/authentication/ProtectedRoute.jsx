9a904a6028f27a6e1763903e1a80be08a9e83e05
Ethan Brown <eb934053@gmail.com>
2025-11-30 16:52:40 -0600

    feat: Add authentication system to client
     - Installed jwt-decode npm package
    
     - Requires user token before accessing main app routes
    
     - Developed ProtectedRoute component to validate user token
    
     - Developed Login Form to send user credentials via HTTP to API
    
     - Created Registration Form file.
    
     - Removed empty Issues.jsx file.
    
     - Created userAPI object in services directory.
    
     - Created Context Provider, allows global access to token state.

--- client/src/Components/authentication/ProtectedRoute.jsx
+++ client/src/Components/authentication/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/context"
-import { useEffect } from "react";
 
 export const ProtectedRoute = ({ children }) => {
     const { token } = useAuth();
